5ebebd0e5d445c9196e4777f279668f6ee73fc56
pokokichi13 <95128025+pokokichi13@users.noreply.github.com>
2022-05-19 20:40:03 +1000

    Update StepFive.js

--- src/component/StepFive.js
+++ src/component/StepFive.js
@@ -9,10 +9,11 @@ function StepFive(props) {
         props.contract.store(event.target.setNum.value)
         .then(
             result => {
-                // Step 6 で使用
+                setCheck("Run Step 4. again to check ");
+                // Step 6 で使用 上のsetCheck をコメントしてください
                 // props.contract.on("Stored", (val) => {
                 //    console.log("Event called - " + val.toNumber());
-                    setCheck("Value changed to " + val.toNumber() + ". Run Step 4. again to check ");
+                //    setCheck("Value changed to " + val.toNumber() + ". Run Step 4. again to check ");
                 //});
                 console.log("STEP 5. ");
                 console.log("Calling store(). It writes to chain, so wallet pops up");
